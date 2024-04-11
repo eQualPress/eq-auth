95b8066f915e7413c33321ce361d702e6078bf0e
AlexisVS <alexis.vansan1440@gmail.com>
2024-04-11 16:43:50 +0200

    update

--- eq-auth.php
+++ eq-auth.php
@@ -74,10 +74,8 @@ function eq_auth_wp_login( string $user_login, WP_User $user ): void {
 }
 
 add_action( 'user_register', 'eq_auth_user_registered' );
-/**
- * @throws Exception
- */
 function eq_auth_user_registered( int $user_id ): void {
+
     $wpUser = get_userdata( $user_id );
 
     \wpcontent\Log::report( 'eq_auth_user_registered => $wpUser', [
@@ -96,12 +94,13 @@ function eq_auth_user_registered( int $user_id ): void {
 
     if ( empty( $eqUser ) ) {
         $username = explode( '@', $wpUser->user_email )[0];
+        $password = wp_generate_password();
 
         $userData = [
+            'wordpress_user_id' => $user_id,
             'email'             => $wpUser->user_email,
             'username'          => $username,
-            'password'          => $wpUser->user_pass,
-            'wordpress_user_id' => $user_id,
+            'password'          => $password,
         ];
 
         \wpcontent\Log::report( 'eq_auth_user_registered => $userData', $userData );
@@ -110,8 +109,17 @@ function eq_auth_user_registered( int $user_id ): void {
 
         \wpcontent\Log::report( 'eq_auth_user_registered => $eqWordpressUserSigninResponse', $eqWordpressUserSigninResponse );
     }
+}
 
-    eq_auth_wp_login( $wpUser['user_email'], get_user_by( 'ID', $user_id ) );
+add_action( 'password_reset', 'eq_auth_password_reset', 10, 2 );
+function eq_auth_password_reset( WP_User $user, string $new_pass ): void {
+    $eqUser = \wordpress\User::search( [ 'login', '=', $user->user_email ] )->read( [ 'id' ] );
+
+    \wpcontent\Log::report( 'eq_auth_password_reset => $eqUser', $eqUser );
+
+    if ( $eqUser ) {
+        $eqUser->update( [ 'password' => $new_pass ] );
+    }
 }
 
 add_action( 'wp_logout', 'eq_auth_wp_logout' );
