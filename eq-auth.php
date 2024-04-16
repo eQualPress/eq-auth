cf574ce0da01f7a71a17bbabd5cdb41b0da62c5c
AlexisVS <63750707+AlexisVS@users.noreply.github.com>
2024-04-16 12:22:06 +0200

    Update eq-auth.php

--- eq-auth.php
+++ eq-auth.php
@@ -18,8 +18,9 @@
 use core\Group;
 use equal\auth\AuthenticationManager;
 
+/*
 include_once ABSPATH . '/wp-content/Log.php';
-
+*/
 
 add_action( 'wp_login', 'eq_auth_wp_login', 10, 2 );
 /**
@@ -37,20 +38,25 @@ function eq_auth_wp_login( string $user_login, WP_User $user ): void {
         'groups_ids'
     ] )->first( true );
 
+    /*
     \wpcontent\Log::report( 'eq_auth_wp_login => WP_USER', [
         'user'       => $user,
         'user_email' => $user->user_email
     ] );
     \wpcontent\Log::report( 'eq_auth_wp_login => $user_login', $user_login );
     \wpcontent\Log::report( 'eq_auth_wp_login => $eqUser', $eq_user );
-
+    */
+    
     if ( ! $eq_user ) {
         throw new Exception( "user_not_found", QN_ERROR_INVALID_USER );
     }
 
     $eq_groups = Group::search( [ 'id', 'in', $eq_user['groups_ids'] ] )->read( [ 'name' ] )->get( true );
-    \wpcontent\Log::report( 'eq_auth_wp_login => eqGroups', $eq_groups );
 
+    /*
+    \wpcontent\Log::report( 'eq_auth_wp_login => eqGroups', $eq_groups );
+    */
+    
     $eq_user['groups'] = array_values( array_map( function ( $group ) {
         return $group['name'];
     }, $eq_groups ) );
@@ -77,20 +83,24 @@ function eq_auth_user_registered( int $user_id ): void {
 
     $wpUser = get_userdata( $user_id );
 
+    /*
     \wpcontent\Log::report( 'eq_auth_user_registered => $wpUser', [
         'user_id'  => $user_id,
         'userdata' => $wpUser
     ] );
-
+    */
+    
     $eq_user = \wordpress\User::search( [ 'login', '=', $wpUser->user_email ] )->read( [
         'id',
         'groups_ids'
     ] )->first( true );
 
+    /*
     \wpcontent\Log::report( 'eq_auth_user_registered => $_POST', $_POST );
     \wpcontent\Log::report( 'eq_auth_user_registered => $eqUser', $eq_user );
     \wpcontent\Log::report( 'eq_auth_user_registered => ! $eqUser', ( ! $eq_user ) );
-
+    */
+    
     if ( empty( $eq_user ) ) {
         $username = explode( '@', $wpUser->user_email )[0];
         $password = wp_generate_password();
@@ -102,11 +112,15 @@ function eq_auth_user_registered( int $user_id ): void {
             'password'          => $password,
         ];
 
+        /*
         \wpcontent\Log::report( 'eq_auth_user_registered => $userData', $user_data );
-
+        */
+        
         $eq_wordpress_user_signin_response = \config\eQual::run( 'do', 'wordpress_user_signup', $user_data );
 
+        /*
         \wpcontent\Log::report( 'eq_auth_user_registered => $eqWordpressUserSigninResponse', $eq_wordpress_user_signin_response );
+        */
     }
 }
 
@@ -114,8 +128,10 @@ add_action( 'password_reset', 'eq_auth_password_reset', 10, 2 );
 function eq_auth_password_reset( WP_User $user, string $new_pass ): void {
     $eqUser = \wordpress\User::search( [ 'login', '=', $user->user_email ] )->read( [ 'id' ] );
 
+    /*
     \wpcontent\Log::report( 'eq_auth_password_reset => $eqUser', $eqUser );
-
+    */
+    
     if ( $eqUser ) {
         $eqUser->update( [ 'password' => password_hash( $new_pass, PASSWORD_BCRYPT ) ] );
     }
@@ -133,9 +149,11 @@ function eq_auth_profile_updated( int $user_id ): void {
             'wordpress_user_id'
         ] )->first( true );
 
+        /*
         \wpcontent\Log::report( 'eq_auth_profile_updated => $eqUser', $eq_user );
         \wpcontent\Log::report( 'eq_auth_profile_updated => $wpUser array', $wpUser );
-
+        */
+        
         if ( ( empty( $wpUser ) || ! empty( $wpUser['user_activation_key'] ) ) && ! $eq_user ) {
             return;
         }
@@ -151,8 +169,10 @@ function eq_auth_profile_updated( int $user_id ): void {
             'username'  => $email
         ];
 
+        /*
         \wpcontent\Log::report( 'eq_auth_profile_updated => $wpUser with meta', $wpUser );
-
+        */
+        
         if ( ! empty( $eq_user['wordpress_user_id'] ) ) {
             eQual::run( 'do', 'wordpress_user_update', [
                 'id'        => (int) $eq_user['id'],
